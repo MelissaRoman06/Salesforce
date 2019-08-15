f9cd9a1c172b16f170cadc38d91b253922e7e3a5
John Salazar Pinto <johnpiterzon@gmail.com>
2019-08-15 09:06:21 -0400

    post hardcode

--- src/test/java/steps/PostSteps.java
+++ src/test/java/steps/PostSteps.java
@@ -8,33 +8,17 @@ package steps;
  * Jala Foundation, ("Confidential Information").  You shall not
  * disclose such Confidential Information and shall use it only in
  * accordance with the terms of the license agreement you entered into
- * with Jala Foundation.
+ * with Jala Foundation.  depende injections hooks before scenarios
  */
 
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
-
-import com.sun.xml.internal.ws.wsdl.writer.document.soap.Body;
-import com.sun.xml.internal.ws.wsdl.writer.document.soap.BodyType;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import gherkin.ast.DataTable;
-import gherkin.ast.TableRow;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.RequestSpecification;
-
-import javax.xml.crypto.Data;
-import java.net.Authenticator;
-import java.util.HashMap;
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import static io.restassured.RestAssured.*;
 
@@ -46,27 +30,16 @@ public class PostSteps {
     private String TOKEN_TYPE = "Bearer";
     private String ACCESS_TOKEN = "00D3i000000rfF0!ARUAQOjI70WgjOmdv9Scodrzs2n2rMkx2TzrYyGavZUrONRjbKp0FPDjVqf8onzhY7WHDOtfpSXKTGfk1NbGdMPfbZ4iNcLi";
 
-//    @Given("^post on the account$")
-//    public void aLeadExistsWithId() {
-//        request = new RequestSpecBuilder()
-//                .setBaseUri("https://na112.salesforce.com/services/data/")
-//                .addHeader("Authorization", TOKEN_TYPE.concat(" ").concat(ACCESS_TOKEN))
-//                .build();
-//    }
-
-
-
-
-    @Given("I perform post operation for  with body")
-    public void iPerformPostOperationForWithBody() {
-        String authenticator = "Bearer 00D3i000000rfF0!ARUAQNFhjMwRMqR4orFsAvSIuBch3KbXE8wbNEZO_JHkRKRb6LfSdDsvRSum.GMRnHSy_A6YgyNdeE8FBbAKdUsRRmPk2e6v";
-        request = given().header("Authorization", authenticator);
+    @Given("I build body for post operation")
+    public void iPerformPostOperationForWithBody() throws JSONException {
+        String authenticator = "Bearer 00D3i000000rfF0!ARUAQD6GRMf8qbz9Ckv_GoiD1LArXxZ3TriU9g4xuEokaIcQ2cBaebfOyopphZX3oVY5frxhTH3sAJc0nOmy0n_QT_dAb3nN";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("Name","account444");
+        request = given().headers("Authorization", authenticator,"Content-Type","application/json").body(jsonObject.toString());
     }
 
-    @When("I should the body has name as")
-    public void iShouldTheBodyHasNameAs() {
-          HashMap<String, String> body = new HashMap<>();
-        body.put("Name", "account233");
-        response = given().spec(request).post("https://na112.salesforce.com/001/e?retURL=%2F001%2Fo", body);
+    @When("I send the body and post the account")
+    public void iShouldTheBodyHasNameAs() throws JSONException {
+        response = given().spec(request).post("https://na112.salesforce.com/services/data/v46.0/sobjects/Account");
     }
 }
--- src/test/resources/features/create.feature
+++ src/test/resources/features/create.feature
@@ -2,7 +2,5 @@ Feature: Verify create account
   This feature try to verify the minimun data that is required for do a post
 
   Scenario: verify the minimun data for a post
-    Given I perform post operation for  with body
-    When I should the body has name as
-
-    #"00D3i000000rfF0!ARUAQOjI70WgjOmdv9Scodrzs2n2rMkx2TzrYyGavZUrONRjbKp0FPDjVqf8onzhY7WHDOtfpSXKTGfk1NbGdMPfbZ4iNcLi"
+    Given I build body for post operation
+    When I send the body and post the account
